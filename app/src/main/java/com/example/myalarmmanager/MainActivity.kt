43c8d799d644d3828fb143fc337fa58847600491
yusuf <yusufnew060@gmail.com>
2025-03-17 11:05:54 +0700

    Menambahkan request permission

--- app/src/main/java/com/example/myalarmmanager/MainActivity.kt
+++ app/src/main/java/com/example/myalarmmanager/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.example.myalarmmanager
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myalarmmanager.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(),
+    View.OnClickListener,
+    DatePickerFragment.DialogDateListener,
+    TimePickerFragment.DialogTimeListener {
 
     private var binding: ActivityMainBinding? = null
     private lateinit var alarmReceiver: AlarmReceiver
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         //Listener one time alarm
         binding?.btnOnceDate?.setOnClickListener(this)
@@ -23,16 +48,58 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         alarmReceiver = AlarmReceiver()
     }
 
+    companion object{
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
+    }
+
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.btn_once_date -> {
+                val datePickerFragment = DatePickerFragment()
+                datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
             }
             R.id.btn_once_time -> {
+                val timePickerFragmentOne = TimePickerFragment()
+                timePickerFragmentOne.show(supportFragmentManager, TIME_PICKER_ONCE_TAG)
             }
             R.id.btn_set_once_alarm -> {
+                val onceDate = binding?.tvOnceDate?.text.toString()
+                val onceTime = binding?.tvOnceTime?.text.toString()
+                val onceMessage = binding?.edtOnceMessage?.text.toString()
+                alarmReceiver.setOneTimeAlarm(this, AlarmReceiver.TYPE_ONE_TIME,
+                    onceDate,
+                    onceTime,
+                    onceMessage)
             }
         }
     }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int) {
+        // Siapkan formatter-nya terlebih dahulu
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        // Set textview one
+        binding?.tvOnceDate?.text = dateFormat.format(calendar.time)
+    }
+
+    override fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int) {
+        // Siapkan time formatter-nya terlebih dahulu
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar.set(Calendar.MINUTE, minute)
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        // Set textview berdasarkan tag
+        when (tag) {
+            TIME_PICKER_ONCE_TAG -> binding?.tvOnceTime?.text = dateFormat.format(calendar.time)
+            TIME_PICKER_REPEAT_TAG -> {}
+            else -> {
+            }
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
